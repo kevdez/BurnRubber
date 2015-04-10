1ebe912884b74643649ee850862634b382a99a6b
unknown <nghia@RDSIT03.rds.raildelivery.com>
2015-04-10 11:20:54 -0700

    - Show application version on Action Bar

--- BurnRubber/src/com/raildeliveryservices/burnrubber/LegActivity.java
+++ BurnRubber/src/com/raildeliveryservices/burnrubber/LegActivity.java
@@ -11,68 +11,68 @@ import com.raildeliveryservices.burnrubber.fragments.LegOutboundFragment;
 
 public class LegActivity extends BaseFragmentActivity implements LegListFragment.Callbacks, LegOutboundFragment.Callbacks {
 
-	private long _orderId;
-	private FragmentManager _fm;
-	private FragmentTransaction _ft;
+    private long _orderId;
+    private FragmentManager _fm;
+    private FragmentTransaction _ft;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_activity);
-		
-		Bundle bundle = getIntent().getExtras();
-		_orderId = bundle.getLong(Constants.BUNDLE_PARAM_ORDER_ID);
-		
-		_fm = getSupportFragmentManager();
-		
-		Fragment f = new LegListFragment();
-		f.setArguments(bundle);
-		
-		_ft = _fm.beginTransaction();
-		_ft.replace(R.id.contentFrameLayout, f);
-		_ft.commit();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
 
-	@Override
-	public void onMessageButtonClick() {
+        Bundle bundle = getIntent().getExtras();
+        _orderId = bundle.getLong(Constants.BUNDLE_PARAM_ORDER_ID);
+
+        _fm = getSupportFragmentManager();
+
+        Fragment f = new LegListFragment();
+        f.setArguments(bundle);
+
+        _ft = _fm.beginTransaction();
+        _ft.replace(R.id.contentFrameLayout, f);
+        _ft.commit();
+    }
+
+    @Override
+    public void onMessageButtonClick() {
 
         startActivity(new Intent(OrderActivity.getOriginalMsgIntent()));
-	}
+    }
 
-	@Override
-	public void onDirectionsButtonClick() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void onDirectionsButtonClick() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void onLegOutboundReturnButtonClick() {
-		_fm.popBackStackImmediate();
-	}
+    }
 
-	@Override
-	public void onOutboundFormClick(long legId, int fileNo) {
-		
-		Fragment f = new LegOutboundFragment();
-		Bundle b = new Bundle();
-		b.putLong(Constants.BUNDLE_PARAM_LEG_ID, legId);
+    @Override
+    public void onLegOutboundReturnButtonClick() {
+        _fm.popBackStackImmediate();
+    }
+
+    @Override
+    public void onOutboundFormClick(long legId, int fileNo) {
+
+        Fragment f = new LegOutboundFragment();
+        Bundle b = new Bundle();
+        b.putLong(Constants.BUNDLE_PARAM_LEG_ID, legId);
         b.putInt(Constants.BUNDLE_PARAM_FILE_NO, fileNo);
-		f.setArguments(b);
-		
-		_ft = _fm.beginTransaction();
-		_ft.addToBackStack(null);
-		_ft.replace(R.id.contentFrameLayout, f);
-		_ft.commit();
-	}
+        f.setArguments(b);
 
-	@Override
-	public void onReturnButtonClick() {
-		finish();
-	}
+        _ft = _fm.beginTransaction();
+        _ft.addToBackStack(null);
+        _ft.replace(R.id.contentFrameLayout, f);
+        _ft.commit();
+    }
 
-	@Override
-	public void onOrderImageButtonClick() {
-		/*
+    @Override
+    public void onReturnButtonClick() {
+        finish();
+    }
+
+    @Override
+    public void onOrderImageButtonClick() {
+        /*
 		Fragment f = new OrderImageFragment();
 		Bundle b = new Bundle();
 		b.putLong(Constants.BUNDLE_PARAM_ORDER_ID, _orderId);
@@ -83,5 +83,5 @@ public class LegActivity extends BaseFragmentActivity implements LegListFragment
 		_ft.replace(R.id.contentFrameLayout, f);
 		_ft.commit();
 		*/
-	}
+    }
 }
